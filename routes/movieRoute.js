30b2004257bd2c1ecfd47115f4e3d5df1625615f
Ranjith R <ranju8804@gmail.com>
2023-02-24 09:00:59 +0530

    routes cleared

--- routes/movieRoute.js
+++ routes/movieRoute.js
@@ -6,16 +6,6 @@ router
   .route("/movie")
   .get(getAll)
 
-  .put((req, res, next) => {
-    res.send("put route");
-  })
-  .patch((req, res, next) => {
-    res.send("patch route");
-  })
-  .delete((req, res, next) => {
-    res.send("delete route");
-  });
-
 router.route("/movie/:id").get(getOne);
 router.route("/movie/:movieid").patch(updateMovie);
 router.route("/movie/:movieid").delete(deleteMovie);
